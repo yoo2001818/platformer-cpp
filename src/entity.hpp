13ef60541d4d713258c98f480d4195f4bb877520
Duknam Yoo <yoo2001818@gmail.com>
2024-01-06 23:55:21 +0900

    Add entity methods

--- src/entity.hpp
+++ src/entity.hpp
@@ -10,15 +10,42 @@ public:
   void *get(const entity &pEntity) const;
   void set(entity &pEntity, void *pValue);
   void remove(entity &pEntity);
+
+  void handle_init(entity &pEntity);
+  void handle_destroy(entity &pEntity);
+
+  // Each entity chunk contains the memory spaces that entity's components can
+  // use. This specifies the memory size used by the component for each entity.
+  int memory_size();
 };
 
-class entity {};
+class entity_handle {
+public:
+  int id() const;
+  int version() const;
 
-class entity_chunk {};
+private:
+  int mId;
+  int mVersion;
+};
+
+class entity {
+public:
+  void get_component_memory(void **pPosition);
+  const entity_handle &handle() const;
+
+private:
+  entity_handle mHandle;
+  bool mDeleted;
+  bool mFloating;
+};
+
+class entity_chunk {
+public:
+  void get_component_memory(void **pPosition, int *pStride);
+};
 
 class entity_group {};
-
-class entity_handle {};
 
 class entity_query {};
 

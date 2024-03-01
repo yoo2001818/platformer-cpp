d660007bb8f7f6fab375769311491790ef7fd9ac
Duknam Yoo <yoo2001818@gmail.com>
2024-03-01 23:46:32 +0900

    Add collision, physics, system

--- src/physics.hpp
+++ src/physics.hpp
@@ -3,11 +3,21 @@
 
 #include <glm/fwd.hpp>
 namespace platformer {
-class physics {
+class game;
+class collision {
 public:
   glm::vec3 mMin();
   glm::vec3 mMax();
+};
+class physics {
+public:
   glm::vec3 mVelocity();
+};
+class physics_system {
+public:
+  physics_system();
+  void update(game &pGame, float pDelta);
+  float mGravity;
 };
 } // namespace platformer
 

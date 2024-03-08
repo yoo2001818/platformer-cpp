325300f19849fb9965e27b8fc3d98f7fc2ee4eb3
Duknam Yoo <yoo2001818@gmail.com>
2024-03-08 22:43:23 +0900

    Change main to support Windows

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,7 @@
 #include "game.hpp"
 #include <memory>
 
-int main() {
+int main(int argc, char **argv) {
   auto gameVar = std::make_unique<platformer::game>();
   platformer::application app(std::move(gameVar));
   app.start();

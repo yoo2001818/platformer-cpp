63c2ddb43590a3c449a694ffcdf901141c32c456
Duknam Yoo <yoo2001818@gmail.com>
2024-04-10 23:28:50 +0900

    Add mesh loader

--- src/render/load.cpp
+++ src/render/load.cpp
@@ -1,8 +1,35 @@
 #include "render/load.hpp"
+#include "render/geometry.hpp"
 #include "render/mesh.hpp"
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <stdexcept>
 
 using namespace platformer;
 
 mesh platformer::load_file_to_mesh(const std::string &pFilename) {
-  // TODO
+  Assimp::Importer importer;
+
+  const aiScene *scene = importer.ReadFile(
+      pFilename, aiProcess_Triangulate | aiProcess_JoinIdenticalVertices |
+                     aiProcess_GenNormals | aiProcess_CalcTangentSpace);
+  if (scene == nullptr) {
+    throw std::runtime_error(importer.GetErrorString());
+  }
+
+  // Normally, we would use mNode (the root node) to traverse and copy the
+  // nodes into the registry. However, since we want to just read one mesh
+  // from the scene, let's not do that, but instead read mMeshes to simplify
+  // the implementation.
+  for (int i = 0; i < scene->mNumMeshes; i += 1) {
+    auto &mesh = scene->mMeshes[i];
+    auto &material = scene->mMaterials[mesh->mMaterialIndex];
+    geometry geom;
+    mesh->mVertices;
+    mesh->mNormals;
+    mesh->mTextureCoords;
+    mesh->mTangents;
+    mesh->mNumFaces;
+  }
 }

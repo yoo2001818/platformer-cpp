a8027d5fd264f9172b4d3c0c93e87312ebc1d5b5
Duknam Yoo <yoo2001818@gmail.com>
2024-04-14 23:54:07 +0900

    Add texture methods

--- src/render/texture.cpp
+++ src/render/texture.cpp
@@ -1,3 +1,17 @@
 #include "render/texture.hpp"
 
 using namespace platformer;
+
+texture::texture() {}
+texture::texture(const texture &pValue) {}
+texture::texture(texture &&pValue) {}
+
+texture &texture::operator=(const texture &pValue) {}
+texture &texture::operator=(texture &&pValue) {}
+
+texture::~texture() {}
+
+void texture::prepare(int &pSlot) {}
+void texture::dispose() {}
+
+void texture::init() {}

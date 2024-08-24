19c82c199feb75814cb896d6ece944704fc25fb7
Duknam Yoo <yoo2001818@gmail.com>
2024-08-24 23:46:51 +0900

    Implement gl_array_buffer, gl_element_array_buffer

--- src/render/buffer.cpp
+++ src/render/buffer.cpp
@@ -3,32 +3,43 @@
 
 using namespace platformer;
 
-buffer::buffer(int pType, int pUsage) : mType(pType), mUsage(pUsage) {}
-buffer::~buffer() { this->dispose(); }
+gl_buffer::gl_buffer(int pType, int pUsage) : mType(pType), mUsage(pUsage) {}
+gl_buffer::~gl_buffer() { this->dispose(); }
 
-unsigned int buffer::length() { return this->mByteLength / this->mDataSize; }
+unsigned int gl_buffer::length() { return this->mByteLength / this->mDataSize; }
 
-void buffer::bind() {}
+void gl_buffer::bind() {
+  if (this->mBuffer == -1) {
+    glCreateBuffers(1, &(this->mBuffer));
+  }
+  glBindBuffer(this->mType, this->mBuffer);
+}
 
-void buffer::dispose() {
+void gl_buffer::dispose() {
   if (this->mBuffer != -1) {
     glDeleteBuffers(1, &this->mBuffer);
     this->mBuffer = -1;
   }
 }
 
-void buffer::set(void *pData, unsigned int pLength, unsigned int pDataSize) {
+void gl_buffer::set(void *pData, unsigned int pLength, unsigned int pDataSize) {
   this->mDataSize = pDataSize;
   this->buffer_data(pData, pLength);
 }
 
-void buffer::buffer_data(void *pData, unsigned int pLength) {
+void gl_buffer::buffer_data(void *pData, unsigned int pLength) {
   this->bind();
   glBufferData(this->mType, pLength, pData, this->mUsage);
 }
 
-void buffer::buffer_sub_data(void *pData, unsigned int pOffset,
-                             unsigned int pLength) {
+void gl_buffer::buffer_sub_data(void *pData, unsigned int pOffset,
+                                unsigned int pLength) {
   this->bind();
   glBufferSubData(this->mType, pOffset, pLength, pData);
 }
+
+gl_array_buffer::gl_array_buffer(int pUsage)
+    : gl_buffer(GL_ARRAY_BUFFER, pUsage) {}
+
+gl_element_array_buffer::gl_element_array_buffer(int pUsage)
+    : gl_buffer(GL_ELEMENT_ARRAY_BUFFER, pUsage) {}
--- src/render/buffer.hpp
+++ src/render/buffer.hpp
@@ -2,13 +2,13 @@
 #define __BUFFER_HPP__
 #include <vector>
 namespace platformer {
-class buffer {
+class gl_buffer {
 public:
-  buffer(int pType, int pUsage);
-  virtual ~buffer();
+  gl_buffer(int pType, int pUsage);
+  virtual ~gl_buffer();
 
   unsigned int length();
-  virtual void bind();
+  void bind();
   void dispose();
 
   void set(void *pData, unsigned int pLength, unsigned int pDataSize);
@@ -18,12 +18,20 @@ public:
   void buffer_data(void *pData, unsigned int pLength);
   void buffer_sub_data(void *pData, unsigned int pOffset, unsigned int pLength);
 
-private:
+protected:
   int mType;
   int mUsage;
   int mDataSize = 1;
   int mByteLength = 0;
   unsigned int mBuffer = -1;
+};
+
+class gl_array_buffer : gl_buffer {
+  gl_array_buffer(int pUsage);
+};
+
+class gl_element_array_buffer : gl_buffer {
+  gl_element_array_buffer(int pUsage);
 };
 
 } // namespace platformer

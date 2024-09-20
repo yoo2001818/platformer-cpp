3d7cbb4e641ba3e9f59b6eaf548c5e1bbf18d98c
Duknam Yoo <yoo2001818@gmail.com>
2024-09-20 15:56:48 +0900

    Fix armature bone matrix calculation

--- src/render/armature.cpp
+++ src/render/armature.cpp
@@ -22,7 +22,8 @@ armature_component::bone_matrices(entt::registry &pRegistry) {
   for (auto &bone : this->bones) {
     auto transformVal = pRegistry.try_get<transform>(bone.joint);
     if (transformVal != nullptr) {
-      matrices.push_back(invMatrix * transformVal->matrix_world(pRegistry));
+      matrices.push_back(transformVal->matrix_world(pRegistry) *
+                         bone.inverseBindMatrix);
     } else {
       matrices.push_back(glm::identity<glm::mat4>());
     }
